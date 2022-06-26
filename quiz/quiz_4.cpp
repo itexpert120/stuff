b2ce58e3a8332c403e52435e535858c259f4a457
Zeeshan Ahmad <itexpert120@gmail.com>
2022-06-26 14:20:05 +0500

    Update quiz_4.cpp

--- quiz/quiz_4.cpp
+++ quiz/quiz_4.cpp
@@ -1,53 +1,64 @@
 #include <iostream>
 using namespace std;
 
-void appendList(int *arr, int *size, const int n);
+// function prototypes
+int* appendList(int *arr, int *size, const int n);
 int getFirst(const int *arr);
 int getLast(const int *arr, const int size);
 
-int main() 
-{
-    // dummy array
-    int *array = new int[]{1, 2, 3, 4, 5};
-    
-    // size variable
+int main() {
+    // create an array of size 5
+    int *array = new int[5]{1, 2, 3, 4, 5};
     int size = 5;
-    cout << "Old Array:" << endl;
     
     // print array
+    cout << "Old Array:" << endl;
     for (int i = 0; i < size; i++)
-      cout << array[i] << " ";
+        cout << array[i] << " ";
     cout << endl;
+   
+    // print first and last elements of array
     cout << "First Element: " << getFirst(array) << endl;
     cout << "Last Element: " << getLast(array, size) << endl;
 
-    // append 6 to last of array
-    appendList(array, &size, 6);
+    // new array
+    int *newArray = appendList(array, &size, 6);
     
     // print new array
     cout << "\nNew Array:" << endl;
-    for (int i = 0; i < size; i++)  cout << array[i] << " ";
+    for (int i = 0; i < size; i++)
+        cout << newArray[i] << " ";
     cout << endl;
-    cout << "First Element: " << getFirst(array) << endl;
-    cout << "Last Element: " << getLast(array, size) << endl;
+    
+    // print first and last elements of new array
+    cout << "First Element: " << getFirst(newArray) << endl;
+    cout << "Last Element: " << getLast(newArray, size) << endl;
     return 0;
 }
 
-// function to add an element at end of array
-void appendList(int *arr, int *size, const int n) 
-{
-    // reallocate memory of array to be able to occupy more than size elements
-    arr = (int*) realloc(arr, (*size + 1) * sizeof(int));
-    arr[*size] = n;
+// function to generate new array
+int* appendList(int *arr, int *size, const int n)  {
+    // create a new array of greater size
+    int* newArray = new int[*size + 1];
+    
+    // transfer values of old array into new array
+    for (int i = 0; i < *size + 1; i++)
+        newArray[i] = arr[i];
+    
+    // make last element of newArray = n
+    newArray[*size] = n;
+    
+    // increase size by 1
     ++(*size);
+    return newArray;
 }
 
-// return first element of array
+// funciton to return first element of array
 int getFirst(const int *arr) {
     return arr[0];
 }
 
-return last element of array
+// functio to return last element of array
 int getLast(const int *arr, const int size) {
     return arr[size - 1];
 }
